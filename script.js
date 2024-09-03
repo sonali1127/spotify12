adf2269afca650bbf8d70b52e5774efb8edf2773
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2024-09-03 21:36:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function displayAlbum() {
     let array = Array.from(as);
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
-        if (e.href.includes("/songs/")) {
+        if (e.href.includes("/songs/")&& !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0];
             let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json();
@@ -66,7 +66,7 @@ async function displayAlbums() {
 
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
-        if (e.href.includes("/music/")) {
+        if (e.href.includes("/music/")&& !e.href.includes(".htaccess"))  {
             let folder = e.href.split("/").slice(-2)[0];
             let a = await fetch(`/music/${folder}/info.json`);
             let response = await a.json();
